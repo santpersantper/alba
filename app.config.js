aed9befffa11c3e44711122d641c22d994f7d97b
santpersantper <santi998@gmail.com>
2026-03-13 16:27:31 +0100

    Enable New Architecture (required by reanimated and worklets)

--- app.config.js
+++ app.config.js
@@ -22,7 +22,7 @@ export default {
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "automatic",
-    newArchEnabled: false,
+    newArchEnabled: true,
     splash: {
       image: "./assets/splash-icon.png",
       resizeMode: "contain",
